18727b19a5dc5f2a4b988d2145ccaf513a14e9a6
Sanjula <sanjula8@gmail.com>
2021-05-04 13:21:19 -0500

    added jsx expressions

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,12 +3,16 @@ import './ExpenseItem.css'
 
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Care Insurance';
+    const expenseAmount = 294.67
+    
     return (
         <div className="expense-item">
-            <div>March 28th 2021</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$295.67</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
         );
